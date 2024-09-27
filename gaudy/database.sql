05b7fc6197e643b842f5da84e9fad8ed3e646430
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-09-27 19:14:24 +0200

    update

--- gaudy/database.sql
+++ gaudy/database.sql
@@ -1,9 +1,9 @@
 create database if not exists todo;
 use todo;
 
-create table todo_tasks {
+create table todo_tasks (
     id_todo int primary key auto_increment,
     task_name varchar(255) not null,
     task_description text not null,
     task_deadline datetime
-};
+);

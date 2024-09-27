c21558e9b984624176bbf2651ff661ca29a3f180
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-09-27 20:43:04 +0200

    update

--- gaudy/database.sql
+++ gaudy/database.sql
@@ -4,5 +4,6 @@ USE todo;
 CREATE TABLE IF NOT EXISTS todo_tasks (
     id_todo INT PRIMARY KEY AUTO_INCREMENT,
     task TEXT NOT NULL, 
-    task_deadline DATETIME
+    task_deadline DATETIME,
+    completed TINYINT(1) DEFAULT 0
 );
--- gaudy/index.php
+++ gaudy/index.php
@@ -22,7 +22,7 @@
         include 'db_connection.php';
 
         // Dodawanie nowego zadania do bazy danych
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && !isset($_POST['mark_completed'])) {
             $task = $_POST['todo'];
             $deadline = $_POST['deadline'];
             $query = "INSERT INTO todo_tasks (task, task_deadline) VALUES ('$task', '$deadline')";
@@ -34,12 +34,29 @@
 
         // Wyświetlanie zadań w HTML
         while ($row = mysqli_fetch_assoc($result)) {
-            echo "<li>";
-            echo "<span>" . $row['task'] . " - " . $row['task_deadline'] . "</span>";
-            echo "<form method='GET' action='edit_task.php' style='display:inline;'>
-                    <input type='hidden' name='task_id' value='" . $row['id_todo'] . "'>
-                    <button class='edit_btn'><i class='fa-solid fa-pencil'></i></button>
-                  </form>";
+            $completed_class = isset($row['completed']) && $row['completed'] ? 'completed' : '';
+
+            echo "<li class='$completed_class'>";
+            echo "<div><strong>Zadanie:</strong><br>" . $row['task'] . "</div>";
+            echo "<div><strong>Do kiedy:</strong><br>" . $row['task_deadline'] . "</div>";
+            
+            // Przycisk oznaczenia ukończonego zadania
+            if (!$row['completed']) {
+                echo "<form method='POST' action='mark_completed.php' style='display:inline;'>
+                        <input type='hidden' name='task_id' value='" . $row['id_todo'] . "'>
+                        <button class='complete-btn'><i class='fa-solid fa-check'></i></button>
+                      </form>";
+            }
+
+            // Przycisk edycji zadania (jeśli nie jest oznaczone jako ukończone)
+            if (!$row['completed']) {
+                echo "<form method='GET' action='edit_task.php' style='display:inline;'>
+                        <input type='hidden' name='task_id' value='" . $row['id_todo'] . "'>
+                        <button class='edit_btn'><i class='fa-solid fa-pencil'></i></button>
+                      </form>";
+            }
+
+            // Przycisk usuwania zadania
             echo "<form method='POST' action='delete_task.php' style='display:inline;'>
                     <input type='hidden' name='task_id' value='" . $row['id_todo'] . "'>
                     <button class='delete-btn'><i class='fa-solid fa-trash'></i></button>

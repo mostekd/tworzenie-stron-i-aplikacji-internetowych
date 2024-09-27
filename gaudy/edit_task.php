7492814dada3bda10fad86b09a41ebd3e424d2bf
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-09-27 20:57:52 +0200

    update

--- gaudy/edit_task.php
+++ gaudy/edit_task.php
@@ -1,24 +1,44 @@
-<?php
-include 'db_connection.php';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Task</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/1deffa5961.js" crossorigin="anonymous"></script>
+</head>
+<body>
+<div class="todo-app">
+    <h1>Todo App Edit Task</h1>
 
-if (isset($_GET['task_id'])) {
-    $task_id = $_GET['task_id'];
-    $result = mysqli_query($connection, "SELECT * FROM todo_tasks WHERE id_todo = $task_id");
-    $task = mysqli_fetch_assoc($result);
-
-    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-        $updated_task = $_POST['task'];
-        $updated_deadline = $_POST['deadline'];
-
-        $query = "UPDATE todo_tasks SET task = '$updated_task', task_deadline = '$updated_deadline' WHERE id_todo = $task_id";
-        mysqli_query($connection, $query);
-        header('Location: index.php');
+    <?php
+    include 'db_connection.php';
+    if (isset($_GET['task_id'])) {
+        $task_id = $_GET['task_id'];
+        $result = mysqli_query($connection, "SELECT * FROM todo_tasks WHERE id_todo = $task_id");
+        $task_data = mysqli_fetch_assoc($result);
     }
-}
-?>
 
-<form method="POST">
-    <input type="text" name="task" value="<?php echo $task['task']; ?>" required>
-    <input type="datetime-local" name="deadline" value="<?php echo date('Y-m-d\TH:i', strtotime($task['task_deadline'])); ?>" required>
-    <button type="submit">Update Task</button>
-</form>
+    if (isset($_POST['update_task'])) {
+        $task = $_POST['todo'];
+        $deadline = $_POST['deadline'];
+        $task_id = $_POST['task_id'];
+
+        mysqli_query($connection, "UPDATE todo_tasks SET task = '$task', task_deadline = '$deadline' WHERE id_todo = $task_id");
+        header("Location: index.php");
+    }
+    ?>
+
+    <!-- Formularz edycji zadania -->
+    <form method="POST" action="edit_task.php">
+        <p>Treść zadania:</p>
+        <input type="text" name="todo" placeholder="Edit your todo" value="<?php echo $task_data['task']; ?>" required>
+        <p>Do kiedy:</p>
+        <input type="datetime-local" name="deadline" value="<?php echo date('Y-m-d\TH:i', strtotime($task_data['task_deadline'])); ?>" required>
+        <input type="hidden" name="task_id" value="<?php echo $task_id; ?>">
+        <button type="submit" name="update_task">Zmień wpis</button>
+    </form>
+</div>
+</body>
+</html>

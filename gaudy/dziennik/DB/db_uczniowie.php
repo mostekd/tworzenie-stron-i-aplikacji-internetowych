dc86011942ef56c467a85e280aa16d9172e7b2e7
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-11-07 20:39:20 +0100

    Update db_uczniowie.php

--- gaudy/dziennik/DB/db_uczniowie.php
+++ gaudy/dziennik/DB/db_uczniowie.php
@@ -1,19 +1,20 @@
 <?php
 include('../DB/db_connection.php');
-    class db_uczniowie extends db_connection{
-        function selectUczenWhereKlasa3TI(){
-            $query = "SELECT * FROM `uczen` WHERE id_klasa = 10 ORDER BY nazwisko ASC";
-            $data = mysqli_query($this->connect, $query);
-            if (mysqli_num_rows($data) > 0){
-            return $data;
-            }
-        }
-
-        function selectNauczycielByDataUrodzenia(){
-            $query  = "SELECT * FROM nauczyciel ORDER BY data_urodzenia DESC";
-            $data = mysqli_query($this->connect, $query);
-            if (mysqli_num_rows($data) > 0){
+class db_uczniowie extends db_connection {
+    function selectUczenWhereKlasa3TI() {
+        $query = "SELECT * FROM `uczen` WHERE id_klasa = 10 ORDER BY nazwisko ASC";
+        $data = mysqli_query($this->connect, $query);
+        if (mysqli_num_rows($data) > 0) {
             return $data;
         }
     }
+
+    function selectNauczycielByDataUrodzenia() {
+        $query = "SELECT * FROM nauczyciel ORDER BY data_urodzenia DESC";
+        $data = mysqli_query($this->connect, $query);
+        if (mysqli_num_rows($data) > 0) {
+            return $data;
+        }
+    }
+}
 ?>

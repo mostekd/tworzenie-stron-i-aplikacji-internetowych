f26ef6c479df7a99809b3d1279345488f8b8766d
Dawid Mostowski <dawid-mostowski@o2.pl>
2024-11-07 21:01:14 +0100

    update

--- gaudy/dziennik/DB/db_uczniowie.php
+++ gaudy/dziennik/DB/db_uczniowie.php
@@ -47,5 +47,23 @@ class db_uczniowie extends db_connection {
         return false;
     }
     
+    function usunNauczycieliPrzed1950() {
+        $queryCount = "SELECT COUNT(*) as count FROM `nauczyciele` WHERE `data_urodzenia` < '1950-01-01'";
+        $resultCount = mysqli_query($this->connect, $queryCount);
+        $count = mysqli_fetch_assoc($resultCount)['count'];
+
+        if ($count > 0) {
+            $queryDelete = "DELETE FROM `nauczyciele` WHERE `data_urodzenia` < '1950-01-01'";
+            $resultDelete = mysqli_query($this->connect, $queryDelete);
+
+            if ($resultDelete) {
+                return $count; 
+            } else {
+                return false;
+            }
+        } else {
+            return 0;
+        }
+    }
 }
 ?>

bf5110106638bf1e499d66019c611d7046ce583f
Dawid Mostowski <dawid.mostowski06@proton.me>
2025-02-25 01:58:47 +0100

    Update db_climbing.php

--- Klasa 4/projekt_strona_internetowa/DB/db_climbing.php
+++ Klasa 4/projekt_strona_internetowa/DB/db_climbing.php
@@ -10,5 +10,33 @@
                 return $data;
             }
         }
+
+        function selectRoutesByDifficulty($difficulty_id)
+        {
+            $query = "SELECT * FROM routes WHERE difficulty = '".$difficulty_id."';";
+            $data = mysqli_query($this->connect, $query);
+            if (mysqli_num_rows($data) > 0)
+            {
+                return $data;
+            }
+        }
+
+        function selectDifficulties()
+        {
+            $query = "SELECT * FROM difficulties";
+            $data = mysqli_query($this->connect, $query);
+            if (mysqli_num_rows($data) > 0)
+            {
+                return $data;
+            }
+        }
+
+        function insertRoute($name, $difficulty_id, $description, $location){
+            $query = "INSERT INTO `routes`(`name`, `difficulty_id`, `description`, `location`) VALUES ('".$name."','".$difficulty_id."','".$description."','".$location."');";
+            $data = mysqli_query($this->connect, $query);
+            header('location: ../BO/admin_about_company.php'); 
+            $this->close();
+        }
+
     }
 ?>

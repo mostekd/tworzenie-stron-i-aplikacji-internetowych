febc942de6d9dd3597a6d77a8b9842a2cca0b57a
Dawid Mostowski <dawid.mostowski06@proton.me>
2025-03-01 16:08:54 +0100

    Update script.js

--- Klasa 4/projekt_strona_internetowa/FO/script.js
+++ Klasa 4/projekt_strona_internetowa/FO/script.js
@@ -1,33 +1,4 @@
-function initMap(lat, lng, elementId) {
-    var map = L.map(elementId).setView([lat, lng], 10);
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    }).addTo(map);
-    L.marker([lat, lng]).addTo(map);
-}
-
 document.addEventListener('DOMContentLoaded', function() {
-    if (document.getElementById('map')) {
-        var map = L.map('map').setView([52.237049, 21.017532], 6); // Centered on Poland
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(map);
-
-        var marker;
-        map.on('click', function(e) {
-            var lat = e.latlng.lat;
-            var lng = e.latlng.lng;
-            if (marker) {
-                marker.setLatLng(e.latlng);
-            } else {
-                marker = L.marker(e.latlng).addTo(map);
-            }
-            document.getElementById('latitude').value = lat;
-            document.getElementById('longitude').value = lng;
-        });
-    }
-
-    // Add animations
     const elements = document.querySelectorAll('section, .route, form');
     elements.forEach(element => {
         element.style.opacity = 0;
